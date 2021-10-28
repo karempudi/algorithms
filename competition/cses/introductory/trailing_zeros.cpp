b6636395b6a926cef603dd20d432d677f24d2b63
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2021-10-28 08:06:27 +0200

    Traling zeros done.. interesting problem

--- competition/cses/introductory/trailing_zeros.cpp
+++ competition/cses/introductory/trailing_zeros.cpp
@@ -6,12 +6,19 @@ int main() {
 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout << "Trailing zeros ... " << endl;
+    //cout << "Trailing zeros ... " << endl;
     int n;
 
     cin >> n;
-    cout << "Number: " << n << endl;
+    //cout << "Number: " << n << endl;
+    //
+    // count only 5's 25's 125's
+    int count = 0;
+    for(int i = 5; n / i >= 1; i *= 5) {
+        count += n/i;
+    }
 
+    cout << count << endl;
 
     return 0;
 }
